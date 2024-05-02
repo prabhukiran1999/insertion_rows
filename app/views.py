498d3f0837a727159d0ab7b8bcb028050360faac
prabhukiran1999 <prabhukiran48000@gmail.com>
2024-05-02 17:28:28 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,30 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.db.models.functions import Length
 # Create your views here.
 from app.models import *
 
 def insert_topic(request):
     tn=input()
+    d={'QLOT':Topic.objects.all()}
+    
+    
     TO=Topic.objects.get_or_create(topic_name=tn)[0]
     TO.save()
-    return HttpResponse('TOPIC IS CREATED')
-def insert_WebPages(requset):
+    return render(request,'display_topic.html',d)
+    
+def insert_WebPages(request):
     tn=input()
     n=input()
     u=input()
+    
 
     LTO=Topic.objects.filter(topic_name=tn)
     if LTO:
         TO=LTO[0]
         WO=WebPages.objects.get_or_create(topic_name=TO,name=n,url=u)[0]
         WO.save()
-        return HttpResponse('WEBPAGES CREATED')
+        d={'WOTP':WebPages.objects.all()}
+        return render(request,'display_webpages.html',d)
     else:
-        return HttpResponse('NO SUCH TOPIC')
+        d={'QLOT':Topic.objects.all()}
+        return render(request,'display_topic.html',d)
 
-def insert_AccessRecord(requset):
-    tn=input()
+def insert_AccessRecord(request):
+   ''' tn=input()
     TO=Topic.objects.create(topic_name=tn)[0]
     TO.save()
     
@@ -37,9 +44,18 @@ def insert_AccessRecord(requset):
     d=input()
     a=input()
     AO=AccessRecord.objects.create(name=WO,data=d,author=a)[0]
-    AO.save()
-    return HttpResponse('ACCESSRECORD CREATED')
+    AO.save()'''
+   n=input()
+   WO=WebPages.objects.get(id=n)
+   d=input('enter date')
+   a=input('enter author')
+   AO=AccessRecord.objects.get_or_create(name=WO,date=d,author=a)[0]
+   AO.save()
+   d={'ACRD':AccessRecord.objects.all()}
+   return render(request,'display_accessrecord.html',d)
 
+   return HttpResponse('ACCESSRECORD CREATED')
+ 
 def display_topic(request):
     QLOT=Topic.objects.all()
     d={'QLOT':QLOT}
@@ -48,6 +64,13 @@ def display_topic(request):
 
 def display_webpages(request):
     WOTP=WebPages.objects.all()
+    WOTP=WebPages.objects.filter(topic_name='cricket')
+    WOTP=WebPages.objects.exclude(topic_name='cricket')
+    WOTP=WebPages.objects.all()[::-1]
+    WOTP=WebPages.objects.all().order_by('name')
+    WOTP=WebPages.objects.all().order_by('-name')
+    WOTP=WebPages.objects.all().order_by(Length('name'))
+    WOTP=WebPages.objects.all().order_by(Length('name').desc())
     d={'WOTP':WOTP}
     return render(request,'display_webpages.html',d)
 
